b4695ac6ad190fe3921593ba141c30fb48eb09f5
himanshu <himanshusharma.it.11@gmail.com>
2019-11-19 13:38:07 +0530

    user table updated

--- database/scripts/sample-data.sql
+++ database/scripts/sample-data.sql
@@ -8,8 +8,8 @@ INSERT INTO `userrole` (`id`,`name`) VALUES (2,'content creator');
 INSERT INTO `userrole` (`id`,`name`) VALUES (3,'user');
 
 delete from user where id > 0;
-INSERT INTO `user` (`id`,`firstName`,`lastName`,`email`,`alternateEmail`,`password`,`residentialAddress`,`imageUrl`,`adharUrl`,`passportUrl`,`phone`,`country`,`roleId`,`workingStatus`,`loginStatus`,`regDate`,`lastLogin`,`lastSeen`,`phoneOTP`) VALUES (1,'Super','Admin','sa@aeon.com','sa2@aeon.com','1234','noida',NULL,NULL,NULL,'8006619568','india',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-UPDATE `user` SET `roleId` = '1', `workingStatus` = 'active' WHERE (`id` = '1');
+INSERT INTO `lokpoll`.`user` (`firstName`, `lastName`, `email`, `password`, `roleId`, `workingStatus`, `loginStatus`, `avatarBG`) VALUES ('super', 'admin', 'sa@lokpoll.com', '1234', '1', 'active', 'online', '#4c5264');
+
 
 INSERT INTO `lokpoll`.`language` (`name`, `code`, `isActive`) VALUES ('odiya', 'or', '1');
 INSERT INTO `lokpoll`.`language` (`name`, `code`, `isActive`) VALUES ('hindi', 'hi', '1');
