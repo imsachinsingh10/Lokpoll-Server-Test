5967e64bd40909705f55f28ccb7befd8d9ed648c
ritu <ritu@aeonlogiciel.com>
2019-11-19 16:01:53 +0530

    language table data type change

--- database/scripts/001_language_table_added.sql
+++ database/scripts/001_language_table_added.sql
@@ -6,7 +6,7 @@ insert into db_logs (`dev`, scriptName) values ('himanshu', '001_language_table_
 DROP TABLE IF EXISTS `language`;
 CREATE TABLE `language` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` bigint(20) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `code` varchar(100) DEFAULT NULL,
   `isActive` tinyint(2) DEFAULT 0,
   `addedOn` datetime DEFAULT NULL,
