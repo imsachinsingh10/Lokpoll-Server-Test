dea56438efad33e43b7dbba26b3521b2ecaf3dca
ritu <ritu@aeonlogiciel.com>
2019-11-20 12:58:15 +0530

    Database tables for content creations

--- database/scripts/003_content_creation_tables_created.sql
+++ database/scripts/003_content_creation_tables_created.sql
@@ -1,53 +1,88 @@
 insert into db_logs (`dev`, scriptName) values ('himanshu', '003_content_creation_tables_created.sql');
 
+-- -----------------------------------------------------
+-- Table `post`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) DEFAULT 0,
+  `moodId` bigint(20) DEFAULT 0,
+  `description` longtext DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `latitude` varchar(100) DEFAULT NULL,
+  `longitude` varchar(100) DEFAULT NULL,
+  `isActive` tinyint(2) DEFAULT 0,
+  `isVerified` tinyint(2) DEFAULT 0,
+  `verifiedBy` bigint(20) DEFAULT 0,
+  `verifiedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
-post
-	id,
-	userId,
-	moodId,
-	description,
-	createdAt
-	lattitude,
-	longitude
-	isActive
-	isVerified
-	verifiedBy
-	verifiedAt
 
-mood
-	id
-	name
-	createdAt
-	createdBy
-	description
+-- -----------------------------------------------------
+-- Table `mood`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mood`;
+CREATE TABLE `mood` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `createdBy` bigint(20) DEFAULT 0,
+  `description` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
-post_media
-	id
-	postId
-	commentId
-	type (image | video)
-	url
+-- -----------------------------------------------------
+-- Table `post_media`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post_media`;
+CREATE TABLE `post_media` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `postId` bigint(20) DEFAULT 0,
+   `commentId` bigint(20) DEFAULT 0,
+   `type` varchar(100) DEFAULT NULL,
+   `url` longtext DEFAULT NULL,
+   PRIMARY KEY (`id`)
+);
 
-post_location
-	id
-	postId
-	lattitude
-	longitude
-	address
-	pinCode
+-- -----------------------------------------------------
+-- Table `post_location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post_location`;
+CREATE TABLE `post_location` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `postId` bigint(20) DEFAULT 0,
+  `latitude` varchar(100) DEFAULT NULL,
+  `longitude` varchar(100) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `pinCode` varchar(100) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+);
 
-post_reaction
-	id
-	postId
-	reactedBy
-	type (respect, )
-	reactedAt
+-- -----------------------------------------------------
+-- Table `post_reaction`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post_reaction`;
+CREATE TABLE `post_reaction` (
+   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `postId` bigint(20) DEFAULT 0,
+   `reactedBy` bigint(20) DEFAULT 0,
+   `type` varchar(100) DEFAULT NULL,
+   `reactedAt` datetime DEFAULT NULL,
+   PRIMARY KEY (`id`)
+);
 
-post_comment
-	id
-	postId
-	userId
-	comment
-	createdAt
-	replyToCommentId
-
+-- -----------------------------------------------------
+-- Table `post_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post_comment`;
+CREATE TABLE `post_comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `postId` bigint(20) DEFAULT 0,
+  `replyToCommentId` bigint(20) DEFAULT 0,
+  `userId` bigint(20) DEFAULT 0,
+  `comment` longtext DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
