05e714dc103587bc6700b678f8e18a8e9075326f
himanshu <himanshusharma.it.11@gmail.com>
2020-02-10 16:02:35 +0530

    version updated

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 import Utils from "./src/service/common/utils";
 import {extractThumbnailFromImage} from "./src/middleware/thumbnail.middleware";
 import path from 'path';
+const { exec } = require('child_process');
 
-const file = {
-    filename: 'abcd.jpeg',
-    path: path.resolve('uploads', 'abcd.jpeg')
-};
-
-extractThumbnailFromImage(file).then(result => {
-    console.log('result', result);
+const ls = exec('ipconfig', function (error, stdout, stderr) {
+    if (error) {
+        console.log(error.stack);
+        console.log('Error code: '+error.code);
+        console.log('Signal received: '+error.signal);
+    }
+    console.log('Child Process STDOUT: '+stdout);
+    console.log('Child Process STDERR: '+stderr);
 });
 
-console.log(Utils.getNumericDate());
+ls.on('exit', function (code) {
+    console.log('Child process exited with exit code '+code);
+});
