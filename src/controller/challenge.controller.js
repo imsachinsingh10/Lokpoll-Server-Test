d21f1d5a92c9d5a17f97f7fed40eee00929fc6fd
aeon <ritu@aeonlogiciel.com>
2020-07-03 12:19:15 +0530

    Some Apis Chnages

--- src/controller/challenge.controller.js
+++ src/controller/challenge.controller.js
@@ -9,11 +9,13 @@ import Validator from "../service/common/validator.service";
 import {ErrorModel} from "../model/common.model";
 import Utils from "../service/common/utils";
 import {ChallengeService} from "../service/challenge.service";
+import {MinIOService} from "../service/common/minio.service";
 
 export class ChallengeController {
     constructor() {
         this.challengeService = new ChallengeService();
         this.postService = new PostService();
+        this.minioService = new MinIOService();
     }
 
 
@@ -35,6 +37,7 @@ export class ChallengeController {
         const files = req.files;
         const entries = {
             description: reqBody.description,
+            challengeId: reqBody.challengeId,
             userId: reqBody.userId || req.user.id,
             creatorId: req.user.id,
             createdAt: 'utc_timestamp()',
@@ -132,8 +135,196 @@ export class ChallengeController {
                 thumbnailUrl: file.thumbnailUrl || null
             }));
             const query = QueryBuilderService.getMultiInsertQuery(table.challengeEntriesMedia, challengeEntriesMedia);
-            await this.challengeServiceService.updateChallengeEntriesUpload(challengeEntryId);
+            await this.challengeService.updateChallengeEntriesUpload(challengeEntryId);
             return SqlService.executeQuery(query);
         }
+    }
+
+    async formatChallengeEntries(req, rawEntries) {
+        if (_.isEmpty(rawEntries)) {
+            return [];
+        }
+        const challengeEntryIds = _.map(rawEntries, r => r.id);
+        const uniqChallengeEntryIds = _.uniq(challengeEntryIds);
+        if (challengeEntryIds.length !== uniqChallengeEntryIds.length) {
+            console.log('+++++++++ alert +++, if someone see this log tell himanshu immediately');
+        }
+        const [comments, subMoods, respects, reactions, trusts, mediaList, challengeEntriesViews] = await Promise.all([
+            this.challengeService.getComments(uniqChallengeEntryIds),
+            this.postService.getSubMoodByPostId(uniqChallengeEntryIds),
+            this.postService.getRespects(uniqChallengeEntryIds),
+            this.postService.getPostReactions(uniqChallengeEntryIds),
+            this.postService.getPostTrust(uniqChallengeEntryIds),
+            this.challengeService.getChallengeEntryMedia(uniqChallengeEntryIds),
+            this.challengeService.getChallengeEntriesViews(uniqChallengeEntryIds),
+        ])
+
+        const challengeEntries = [];
+        _.forEach(rawEntries, (entries) => {
+            const _entries = this.getChallengeEntries(
+                {userId: req.user.id, entries, comments, challengeEntriesViews, subMoods, respects, reactions, trusts, mediaList}
+            );
+            challengeEntries.push(_entries);
+        });
+        return challengeEntries;
+    }
+
+    getChallengeEntries({userId, entries, comments, challengeEntriesViews, subMoods, respects, reactions, trusts, mediaList}) {
+        const challengeEntriesViewFiltered = challengeEntriesViews.filter(challengeEntriesViews => challengeEntriesViews.challengeEntryId === entries.id);
+
+        let viewCount = 0;
+        let viewedByUsers = [];
+        if (challengeEntriesViewFiltered.length > 0) {
+            viewCount = challengeEntriesViewFiltered.length;
+            viewedByUsers = challengeEntriesViewFiltered.map(p => {
+                return {
+                    id: p.userId,
+                    name: p.userName,
+                    imageUrl: p.userImageUrl,
+                }
+            })
+        }
+
+        const postComments = comments.filter(comment => comment.postId === entries.id);
+        const subMoodData = subMoods.filter(subMood => subMood.postId === entries.id);
+        const basicDetails = this.getBasicChallengeEntriesDetails(userId, entries, respects);
+        const media = mediaList
+            .filter(m => m.challengeEntryId === entries.id && m.url !== null && m.commentId === 0)
+            .map(p => ({
+                type: p.type,
+                url: p.url,
+                thumbnailUrl: p.thumbnailUrl
+            }));
+        const formattedComments = this.getFormattedComments(postComments);
+
+        const {
+            reaction, loveCount, angryCount, enjoyCount, lolCount, wowCount, sadCount,
+            trust, voteUpCount, voteDownCount, noVoteCount,
+        } = this.getReactionsWithCount(userId, entries, reactions, trusts);
+        return {
+            ...basicDetails,
+            viewCount,
+            viewedByUsers,
+            subMood: subMoodData,
+            media,
+            trustMeter: {
+                voteUpCount, voteDownCount, noVoteCount,
+                trustByMe: _.isEmpty(trust) ? null : trust.type,
+            },
+            reactions: {
+                loveCount, angryCount, enjoyCount, lolCount, wowCount, sadCount,
+                reactionByMe: _.isEmpty(reaction) ? null : reaction.type,
+            },
+            comments: formattedComments,
+            commentCount: formattedComments.length
+        }
+    }
+
+    getBasicChallengeEntriesDetails(userId, entries, respects) {
+        const respectedByMe = _.find(respects, (r) => {
+            return userId === r.respectBy && entries.userId === r.respectFor;
+        });
+        return {
+            id: entries.id,
+            distanceInMeters: Utils.getDistanceInMeters(entries.distance),
+            createdAt: Utils.getNumericDate(entries.createdAt),
+            description: entries.description,
+            type: entries.postType,
+            mood: entries.mood,
+            source: entries.source,
+            language: entries.language,
+            languageCode: entries.languageCode,
+            linkToShare: "https://www.socialmediatoday.com",
+            user: {
+                id: entries.userId,
+                displayName: entries.displayName || entries.userName,
+                profileType: entries.profileType || ProfileType.personal,
+                imageUrl: entries.imageUrl,
+                bgImageUrl: entries.bgImageUrl,
+                audioUrl: entries.audioUrl,
+                respectedByMe: !_.isEmpty(respectedByMe),
+            },
+            location: {
+                latitude: entries.latitude,
+                longitude: entries.longitude,
+                address: entries.address
+            }
+        }
+    }
+
+    getFormattedComments(comments) {
+        const final = [];
+        while (!_.isEmpty(comments)) {
+            const consumedCommentIds = [];
+            final.push(this.getFormattedComment(comments[0], comments, consumedCommentIds));
+            comments = comments.filter(c => consumedCommentIds.indexOf(c.id) < 0)
+        }
+        return final;
+    }
+
+    getFormattedComment(comment, allComments, consumedCommentIds) {
+        consumedCommentIds.push(comment.id);
+        return {
+            id: comment.id,
+            comment: comment.comment,
+            media: {
+                type: comment.type,
+                url: comment.url
+            },
+            user: {
+                name: comment.name,
+                imageUrl: comment.imageUrl,
+                id: comment.userId
+            },
+            replies: this.getCommentReplies(comment.id, allComments, consumedCommentIds)
+        };
+    }
+
+    getCommentReplies(commentId, allComments, consumedCommentIds) {
+        const comments = allComments.filter(c => c.replyToCommentId === commentId);
+        return comments.map(c => {
+            return this.getFormattedComment(c, allComments, consumedCommentIds)
+        })
+    }
+
+    getReactionsWithCount(userId, post, reactions, trusts) {
+        const trust = _.find(trusts, (r) => {
+            return userId === r.reactedBy && post.id === r.postId;
+        });
+        const voteUpCount = _.filter(trusts, (r) => {
+            return r.postId === post.id && r.type === PostVoteOption.voteUp;
+        }).length;
+        const voteDownCount = _.filter(trusts, (r) => {
+            return r.postId === post.id && r.type === PostVoteOption.voteDown;
+        }).length;
+        const noVoteCount = _.filter(trusts, (r) => {
+            return r.postId === post.id && r.type === PostVoteOption.noVote;
+        }).length;
+
+        const reaction = _.find(reactions, (r) => {
+            return userId === r.reactedBy && post.id === r.postId;
+        });
+        const loveCount = _.filter(reactions, (r) => {
+            return r.postId === post.id && r.type === PostReaction.love;
+        }).length;
+        const angryCount = _.filter(reactions, (r) => {
+            return r.postId === post.id && r.type === PostReaction.angry;
+        }).length;
+        const enjoyCount = _.filter(reactions, (r) => {
+            return r.postId === post.id && r.type === PostReaction.enjoy;
+        }).length;
+        const lolCount = _.filter(reactions, (r) => {
+            return r.postId === post.id && r.type === PostReaction.lol;
+        }).length;
+        const wowCount = _.filter(reactions, (r) => {
+            return r.postId === post.id && r.type === PostReaction.wow;
+        }).length;
+        const sadCount = _.filter(reactions, (r) => {
+            return r.postId === post.id && r.type === PostReaction.sad;
+        }).length;
+        return {
+            reaction, loveCount, angryCount, enjoyCount, lolCount, wowCount, sadCount,
+            trust, voteUpCount, voteDownCount, noVoteCount,
+        };
     }
 }
--- src/service/challenge.service.js
+++ src/service/challenge.service.js
@@ -75,5 +75,110 @@ export class ChallengeService {
         return SqlService.executeQuery(query);
     }
 
+    async getAllChallengeEntries(req) {
+        // new SqlService();
+        const reqCoordinate = {
+            latitude: req.latitude,
+            longitude: req.longitude,
+        };
+        let condition1 = ``;
+        let condition2 = ``;
+        let condition3 = ``;
+        let condition4 = '';
+        let distanceQuery = ``;
+        let havingCondition = ``;
+
+        if (req.languageCode) {
+            condition4 = `and ce.languageCode = '${req.languageCode}'`;
+        }
+        if (req.entryByUserId > 0) {
+            condition2 = `and ce.userId = ${req.entryByUserId}`;
+            condition4 = '';
+        }
+        if (req.challengeId) {
+            condition4 = `and ce.challengeId = '${req.challengeId}'`;
+        }
+        try {
+            const moodIds = JSON.parse(req.moodIds);
+            if (!_.isEmpty(req.moodIds) && Array.isArray(moodIds)) {
+                condition3 = `and ce.moodId in ${Utils.getRange(moodIds)}`
+            }
+        } catch (e) {
+
+        }
+
+        if (req.latitude && req.longitude && req.radiusInMeter) {
+            havingCondition = `having distance <= ${Utils.getDistanceInMiles(req.radiusInMeter)}`;
+            distanceQuery = `, SQRT(
+                                POW(69.1 * (ce.latitude - ${reqCoordinate.latitude}), 2) +
+                                POW(69.1 * (${reqCoordinate.longitude} - ce.longitude) * COS(ce.latitude / 57.3), 2)
+                            ) AS distance`
+        }
+        const query = `select ce.id, ce.createdAt, ce.description, ce.source, 
+                            ce.latitude, ce.longitude, ce.address, l.name language, ce.languageCode,
+                            0 'respects', 0 'comments',
+                            ce.type 'postType',
+                            pro.name 'displayName', pro.type 'profileType',
+                            u.id userId, u.name userName, u.imageUrl, u.bgImageUrl, u.audioUrl,
+                            m.${req.languageCode || 'en'} 'mood'
+                            ${distanceQuery}
+                        from ${table.challengeEntries} ce 
+                            join user u on u.id = ce.userId
+                            left join mood m on m.id = ce.moodId
+                            left join profile pro on pro.type = ce.profileType and pro.userId = u.id
+                            left join language l on l.code = ce.languageCode
+                        where 
+                            ce.isDeleted = 0
+                            and ce.latitude is not null and ce.longitude is not null
+                            and ce.isPostUpload = 1 
+                            ${condition1} ${condition2} ${condition3} ${condition4}
+                            ${havingCondition}
+                        order by ce.id desc
+                        limit ${req.postCount} offset ${req.offset}
+                        ;`;
+        return SqlService.executeQuery(query);
+    }
+
+    async getComments(entryIds) {
+        const query = `select pc.*, u.name, u.imageUrl, u.id userId, pm.url, pm.type
+                        from ${table.challengeEntriesComment} pc
+                            left join user u on u.id = pc.userId
+                            left join ${table.challengeEntriesMedia} pm on pm.commentId = pc.id
+                        where 
+                            pc.isDeleted = 0
+                            and pc.challengeEntryId in ${Utils.getRange(entryIds)};`;
+        return SqlService.executeQuery(query);
+    }
+
+    async getChallengeEntryMedia(uniqChallengeEntryIds) {
+        const query = `select pm.challengeEntryId, pm.type, pm.url, pm.thumbnailUrl, pm.commentId from ${table.challengeEntriesMedia}  pm
+                        where challengeEntryId in ${Utils.getRange(uniqChallengeEntryIds)}`;
+        return SqlService.executeQuery(query);
+    }
+
+    async getChallengeEntriesViews(uniqChallengeEntryIds) {
+        const query = `select pv.*, u.name userName, u.imageUrl userImageUrl, u.id userId from 
+                        ${table.challengeEntriesView} pv 
+                            inner join user u on u.id = pv.userId 
+                        where challengeEntryId in ${Utils.getRange(uniqChallengeEntryIds)};`;
+        return SqlService.executeQuery(query);
+    }
+
+    async getChallengeEntriesData(req) {
+        const query = `select p.id, p.createdAt, p.description, p.source, p.latitude, p.longitude, p.address, p.language,
+                            0 'respects', 0 'comments',
+                            p.type 'postType',
+                            pro.name 'displayName', pro.type 'profileType',
+                            u.id userId, u.name userName, u.imageUrl, u.bgImageUrl, u.audioUrl
+                        from ${table.challengeEntries} p 
+                            join user u on u.id = p.userId
+                            left join mood m on m.id = p.moodId
+                            left join profile pro on pro.type = p.profileType and pro.userId = u.id
+                        where 
+                            p.isDeleted = 0
+                            and p.id = ${req.challengeEntryId}
+                        ;`;
+        return SqlService.executeQuery(query);
+    }
 
 }
