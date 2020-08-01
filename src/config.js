72fbd7d35a383b9ab6a5705f9e9e3ebf4827b59e
himanshu <himanshu@aeonlogiciel.com>
2020-08-01 12:46:19 +0000

    updated

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import {Environment} from "./enum/common.enum";
 
 export class Config {
 
-    static env = Environment.test;
+    static env = Environment.prod;
 
     static version = {
         majorRevision: 4,  // (new UI, lots of new features, conceptual change, etc.)
