5c785c7fbc7f7361201de4af1449543e6e64855d
himanshu <himanshu@aeonlogiciel.com>
2020-08-06 09:09:28 +0000

    updated

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 import {SMSService} from "./src/service/common/sms.service";
 import path from 'path';
 
-// SMSService.sendTestSMS();
+const text = 'j;alsdf j;l https://youtu.be/atcqKyQgmVo his f';
+if (text.indexOf('www.youtube.com') > -1 || text.indexOf('youtu.be') > -1) {
+    const parts = text.split(' ');
+    console.log('parts', parts);
+    for (let part of parts) {
 
-
-console.log(path.resolve('assets'))
+    }
+}
