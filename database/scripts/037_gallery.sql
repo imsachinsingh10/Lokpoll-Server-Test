f7a4f3cb9f17504c5868b5f87bdd234afce4555c
imsachinsingh10 <sachin.99dps@gmail.com>
2021-01-30 22:26:12 +0530

    gallery sql changes

--- database/scripts/037_gallery.sql
+++ database/scripts/037_gallery.sql
@@ -1,7 +1,12 @@
+insert into db_logs (`dev`, scriptName) values ('himanshu', '037_gallery.sql');
+
+drop table if exists `gallery`;
+
 CREATE TABLE `gallery` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `fileName` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
-  `createdDate` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `createdDate` datetime NOT NULL DEFAULT current_timestamp(),
+PRIMARY KEY (`id`)  
+)
